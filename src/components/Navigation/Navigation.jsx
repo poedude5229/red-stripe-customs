8604e0c042805c926b5936a0e7d7557112252e7b
Joseph <poedude5229@gmail.com>
2025-04-19 20:14:58 -0400

    portioning in progress, navigation bar started

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,8 +1,43 @@
 import { NavLink } from "react-router-dom";
 import { useState, useEffect, useRef } from "react";
-
+import favicon from "../../favicon.png";
+import "./Navigation.css"
 export default function Navigation() {
   const [showMenu, setShowMenu] = useState(true);
 
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <ul id="topNavbar">
+          <li id="home-navlink">
+            <NavLink id="homenavlinkchild" to="/">
+              <img src={favicon} alt="Home" />
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+      <p>asdjlkfjlksdaf</p>
+      <p>asdjlkfjlksdaf</p>
+      <p>asdjlkfjlksdaf</p>
+      <p>asdjlkfjlksdaf</p>
+      <p>asdjlkfjlksdaf</p>
+      <p>asdjlkfjlksdaf</p>
+      <p>asdjlkfjlksdaf</p>
+      <p>asdjlkfjlksdaf</p>
+      <p>asdjlkfjlksdaf</p>
+      <p>asdjlkfjlksdaf</p>
+      <p>asdjlkfjlksdaf</p>
+      <p>asdjlkfjlksdaf</p>
+      <p>asdjlkfjlksdaf</p>
+      <p>asdjlkfjlksdaf</p>
+      <p>asdjlkfjlksdaf</p>
+      <p>asdjlkfjlksdaf</p>
+      <p>asdjlkfjlksdaf</p>
+      <p>asdjlkfjlksdaf</p>
+      <p>asdjlkfjlksdaf</p>
+      <p>asdjlkfjlksdaf</p>
+      <p>asdjlkfjlksdaf</p>
+      <p>asdjlkfjlksdaf</p>
+    </div>
+  );
 }
